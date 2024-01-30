8c615e0ac6051a3972ab088c45aca20e93d69dca
Tsotne Tsirekidze <tsirekidze022@gmail.com>
2024-01-31 01:41:07 +0400

    cleanup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const connectToDatabase = require("./db"); // Import the connectToDatabase function
+const connectToDatabase = require("./db"); 
 require("dotenv").config();
 
 const app = express();
