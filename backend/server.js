95586534b2699e5d98ff6c0e18d82d77343f991c
Tsotne Tsirekidze <tsirekidze022@gmail.com>
2024-01-31 00:58:34 +0400

    db.js modified

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient } = require("mongodb");
+const connectToDatabase = require("./db"); // Import the connectToDatabase function
+require("dotenv").config();
 
 const app = express();
 const port = 3001;
 
 app.use(cors());
-
-const uri =
-  "mongodb+srv://tsirekidze022:tsirekidze99@cluster0.fulnsyr.mongodb.net/firstDb?retryWrites=true&w=majority";
-
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-async function connectToDatabase() {
-  try {
-    await client.connect();
-    console.log("Connected to MongoDB");
-    return client.db();
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    throw error;
-  }
-}
 
 app.get("/api/restaurants", async (req, res) => {
   try {
