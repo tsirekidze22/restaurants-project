f34026dd6389c01590b5274e542a303c8a6e946e
Tsotne Tsirekidze <tsirekidze022@gmail.com>
2024-01-31 01:58:34 +0400

    index.js updated

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,15 @@ require("dotenv").config();
 
 const app = express();
 const port = 3001;
+const cors = require("cors");
 
+app.use(
+  cors({
+    origin: ["https://res-os-test-frontend.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.get("/", (req, res) => {
   res.json("Success");
 });
