f6688d9933cf843b88073547f8d1c6dfb71be418
Tsotne Tsirekidze <tsirekidze022@gmail.com>
2024-02-03 20:06:08 +0400

    running script changed

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,19 @@
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
+const express = require("express");
 
 const uri = process.env.MONGODB_URI;
+const app = express();
+
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: ["https://res-os-test-frontend.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
